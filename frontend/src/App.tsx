2dee514b0db2b1c54f97e68437ef54ef5ea108e1
jamespd10 <jamespd10@gmail.com>
2021-01-10 00:15:02 -0500

    [ADD] archivos de inicio agregados

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import './App.css';
 //import Routes from './routes/';
 import 'line-awesome/dist/line-awesome/css/line-awesome.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
 const Routes = React.lazy(() => import('./routes'));
 
 const loginUser = React.createContext(false);
@@ -9,6 +10,7 @@ const loginUser = React.createContext(false);
 function App() {
   return (
     <loginUser.Provider value={false}>
+      <CssBaseline />
       {/*render ? <div className="loader-page"></div> : <Routes />*/}
       <Suspense fallback={<div className="loader"></div>}>
         <Routes />
